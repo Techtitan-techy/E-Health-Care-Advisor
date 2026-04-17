00c63283568748d4e7641c44906be583ce53c10f
karthikesh <karthikesh04.m@gmail.com>
2026-04-17 10:57:29 +0530

    Update middleware to exclude .txt files from analysis

--- middleware.js
+++ middleware.js
@@ -64,6 +64,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * - any file with an extension like .css, .js, .png, .jpg, etc.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|css|js)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|css|js|txt)$).*)',
   ],
 };
